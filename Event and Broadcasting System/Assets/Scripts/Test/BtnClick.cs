b04c6fcf117802140f1eb15f4d716ce7b0cba5f8
hezhangqiang1 <1025440064@qq.com>
2019-01-26 17:44:28 +0800

    事件的监听与广播系统

---
+++ Event and Broadcasting System/Assets/Scripts/Test/BtnClick.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 按钮的监听方法 点击按钮的时候广播show方法
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnClick : MonoBehaviour {
+
+	
+	void Awake () {
+        Button btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(()=>
+          EventCenter.Broadcast(EventType.ShowText,"你好，世界")
+            );
+	}
+	
+}
---
+++ Event and Broadcasting System/Assets/Scripts/Test/ShowText.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * 给Text注册事件监听与移除事件监听
+ * 
+ * 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShowText : MonoBehaviour {
+
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+        EventCenter.AddListener<string>(EventType.ShowText, Show);
+    }
+	private void OnDestroy()
+    {
+        EventCenter.RemoveListener<string>(EventType.ShowText, Show);
+    }
+	private void Show(string str)
+    {
+        gameObject.SetActive(true);
+        gameObject.GetComponent<Text>().text = str;
+    }
+}
